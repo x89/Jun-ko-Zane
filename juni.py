4167f4100c36d96fce564b8519259bba951400e9
x89 <napalm10@gmail.com>
2015-05-26 16:36:29 +0100

    Randint and other fun

--- juni.py
+++ juni.py
@@ -2,6 +2,7 @@
 
 from juni import Juni
 from time import sleep
+from random import randint
 import configparser
 import asyncio
 import websockets
@@ -10,22 +11,24 @@ config = configparser.ConfigParser()
 config.read('juni.conf')
 
 ids = (config.getint('DEFAULT', 'first_id'), config.get('DEFAULT', 'second_id'))
+ids = (randint(100, 999), randint(100000, 999999))
 
 j = Juni(ids)
 wss_url = "wss://notifications.freelancer.com/{first_id}/{second_id}/websocket".format(
-    first_id=ids[0],
-    second_id=ids[1],
+    first_id=ids[0],  # Turns out these are just random. 100-999
+    second_id=ids[1],  # This can be a string, no reason not to only use int!
 )
-
-wss_url = "wss://notifications.freelancer.com/info"
-
-# See socks.js
 
 @asyncio.coroutine
 def fl_notifications():
-    ws = yield from websockets.connect(wss_url)
-    ret = yield from ws.recv()
-    print(ret)
+    ws = yield from websockets.client.connect(wss_url)
+    while True:
+        msg = yield from ws.recv()
+        print(msg)
+        if msg == 'o':
+            yield from ws.send('')
+        if not ws.open:
+            break
 
-
+# asyncio.get_event_loop().run_forever()
 asyncio.get_event_loop().run_until_complete(fl_notifications())
