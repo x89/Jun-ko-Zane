0eb17e8d7c0b1414ac7c049f76096cc2f806e3ad
x89 <napalm10@gmail.com>
2015-06-04 22:40:57 +0100

    wee wheoo!

--- juni.py
+++ juni.py
@@ -3,6 +3,7 @@
 from juni import Juni
 from time import sleep
 from random import randint
+import json
 import configparser
 import asyncio
 import websockets
@@ -19,14 +20,18 @@ wss_url = "wss://notifications.freelancer.com/{first_id}/{second_id}/websocket".
     second_id=ids[1],  # This can be a string, no reason not to only use int!
 )
 
+register_blob = b'{"channel":"auth","body":{"hash":"ccc5c539c16c344571bac1f39da804af","hash2":"GIwIMHaIGiESxa5xT0PzoOjA2uXY70UDsy5n95CDllw%3D","user_id":"14809114","channels":[4,13,16,30,31,33,116,150,215,254,270,301,336,421,564,568,588,590,619,620,622,667,683,698,711,741]}}'
+
 @asyncio.coroutine
 def fl_notifications():
     ws = yield from websockets.client.connect(wss_url)
-    while True:
-        msg = yield from ws.recv()
-        print(msg)
-        if not ws.open:
-            break
+    ws.send(register_blob)
+    msg = yield from ws.recv()
+    print(msg)
+    msg = yield from ws.recv()
+    print(msg)
+    msg = yield from ws.recv()
+    print(msg)
 
-# asyncio.get_event_loop().run_forever()
+#asyncio.get_event_loop().run_forever()
 asyncio.get_event_loop().run_until_complete(fl_notifications())
